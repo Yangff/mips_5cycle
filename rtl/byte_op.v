2fee630f6fa0e17a8ec18301f1e87b52540594b1
Fangfei Yang <yangff1@gmail.com>
2016-11-14 16:56:59 +0800

    byte-wise

--- rtl/byte_op.v
+++ rtl/byte_op.v
@@ -2,21 +2,27 @@
 // big-editon
 module byte_op(org, pos, val, result);
 
-    input [31:0] org;
-    input [1:0] pos;
-    input [7:0] val;
+	input [31:0] org;
+	input [2:0] pos;
+	input [7:0] val;
 
-    output [31:0] result;
+	output [31:0] result;
 
-    always @(*) begin
-      case (pos) begin
-        4'b00: result = {val, org[23:0]}; // 00 - lower memory, higher data 
-        4'b01: result = {org[31:24], val, org[15:0]};
-        4'b10: result = {org[31:16], val, org[7:0]};
-        4'b11: result = {org[31:8], val}; 
-        default: result = 0;
-      endcase
-    end
+	always @(*) begin
+		result = 0;
+		case (pos)
+		  4'b100: result = {val, org[23:0]}; // 00 - lower memory, higher data 
+		  4'b101: result = {org[31:24], val, org[15:0]};
+		  4'b110: result = {org[31:16], val, org[7:0]};
+		  4'b111: result = {org[31:8], val}; 
+
+		  4'b000: result = org[31:24];
+		  4'b001: result = org[23:16];
+		  4'b010: result = org[15:8];
+		  4'b011: result = org[7:0];
+		  default: result = 0;
+		endcase
+	end
 endmodule
 
 /*
@@ -25,21 +31,25 @@ little-editon
 
 module byte_op(org, pos, val, result);
 
-    input [31:0] org;
-    input [1:0] pos;
-    input [7:0] val;
+	input [31:0] org;
+	input [2:0] pos;
+	input [7:0] val;
 
-    output [31:0] result;
+	output [31:0] result;
 
-    always @(*) begin
-      case (pos) begin
-        4'b00: result = {org[31:8], val};; // 00 - lower memory, lower data 
-        4'b01: result = {org[31:16], val, org[7:0]};
-        4'b10: result = {org[31:24], val, org[15:0]};
-        4'b11: result = {val, org[23:0]}
-        default: result = 0;
-      endcase
-    end
+	always @(*) begin
+		case (pos) begin
+		  4'b100: result = {org[31:8], val};; // 00 - lower memory, lower data 
+		  4'b101: result = {org[31:16], val, org[7:0]};
+		  4'b110: result = {org[31:24], val, org[15:0]};
+		  4'b111: result = {val, org[23:0]}
+		  4'b000: result = org[7:0];
+		  4'b001: result = org[15:8];
+		  4'b010: result = org[23:16];
+		  4'b011: result = org[31:24];
+		default: result = 0;
+	  endcase
+	end
 endmodule
 
 */
--- rtl/decoder.v
+++ rtl/decoder.v
@@ -42,8 +42,8 @@ module decoder(
 	parameter   BTYPEEX = 4'b1000;	// State 8
 	parameter   ITYPEEX = 4'b1001;	// State 9
 	parameter   ITYPEWB = 4'b1010;	// state 10
-	parameter   JREAD   = 4'b1011;	// State 11
-	parameter   JWRITE  = 4'b1100;	// State 12
+	parameter   JWRITE  = 4'b1011;	// State 11 (write then read)
+	parameter   JREAD   = 4'b1100;	// State 12 (write then read)
 	parameter   JREX     = 4'b1101;	// State 13 (addred from register)
 	parameter   JEX     = 4'b1110;	// State 14
 	
@@ -287,65 +287,65 @@ module decoder(
 	assign dc_op = dc_ins[31:26];
 
 	// 加载
-	assign dc_ins_lb = dc_op == 6'b100000;
-	assign dc_ins_lbu = dc_op == 6'b100100;
-	assign dc_ins_lh = dc_op == 6'b100001;
-	assign dc_ins_lhu = dc_op == 6'b100101;
-	assign dc_ins_lw = dc_op == 6'b100011;
+	assign dc_ins_lb = dc_op == 6'b100000 ?1'b1:1'b0;
+	assign dc_ins_lbu = dc_op == 6'b100100 ?1'b1:1'b0;
+	assign dc_ins_lh = dc_op == 6'b100001 ?1'b1:1'b0;
+	assign dc_ins_lhu = dc_op == 6'b100101 ?1'b1:1'b0;
+	assign dc_ins_lw = dc_op == 6'b100011 ?1'b1:1'b0;
 	assign dc_type_load = dc_ins_lb | dc_ins_lbu | dc_ins_lh | dc_ins_lhu | dc_ins_lw;
 	// 保存
-	assign dc_ins_sb = dc_op == 6'b101000;
-	assign dc_ins_sh = dc_op == 6'b101001;
-	assign dc_ins_sw = dc_op == 6'b101011;
+	assign dc_ins_sb = dc_op == 6'b101000 ?1'b1:1'b0;
+	assign dc_ins_sh = dc_op == 6'b101001 ?1'b1:1'b0;
+	assign dc_ins_sw = dc_op == 6'b101011 ?1'b1:1'b0;
 	assign dc_type_save = dc_ins_sb | dc_ins_sh | dc_ins_sw;
 	// R-R运算
-	assign dc_ins_add = (dc_op == 6'b000000) & (dc_funt == 6'b100000);
-	assign dc_ins_addu = (dc_op == 6'b000000) & (dc_funt == 6'b100001);
-	assign dc_ins_sub = (dc_op == 6'b000000) & (dc_funt == 6'b100010);
-	assign dc_ins_subu = (dc_op == 6'b000000) & (dc_funt == 6'b100011);
-	assign dc_ins_slt = (dc_op == 6'b000000) & (dc_funt == 6'b101010);
-	assign dc_ins_sltu = (dc_op == 6'b000000) & (dc_funt == 6'b101011);
-	assign dc_ins_sll = (dc_op == 6'b000000) & (dc_funt == 6'b000000);
-	assign dc_ins_srl = (dc_op == 6'b000000) & (dc_funt == 6'b000010);
-	assign dc_ins_sra = (dc_op == 6'b000000) & (dc_funt == 6'b000011);
-	assign dc_ins_sllv = (dc_op == 6'b000000) & (dc_funt == 6'b000100);
-	assign dc_ins_srlv = (dc_op == 6'b000000) & (dc_funt == 6'b000110);
-	assign dc_ins_srav = (dc_op == 6'b000000) & (dc_funt == 6'b000111);
-	assign dc_ins_and = (dc_op == 6'b000000) & (dc_funt == 6'b100100);
-	assign dc_ins_or = (dc_op == 6'b000000) & (dc_funt == 6'b100101);
-	assign dc_ins_xor = (dc_op == 6'b000000) & (dc_funt == 6'b100110);
-	assign dc_ins_nor = (dc_op == 6'b000000) & (dc_funt == 6'b100111);
+	assign dc_ins_add = (dc_op == 6'b000000) & (dc_funt == 6'b100000) ?1'b1:1'b0;
+	assign dc_ins_addu = (dc_op == 6'b000000) & (dc_funt == 6'b100001 ?1'b1:1'b0);
+	assign dc_ins_sub = (dc_op == 6'b000000) & (dc_funt == 6'b100010) ?1'b1:1'b0;
+	assign dc_ins_subu = (dc_op == 6'b000000) & (dc_funt == 6'b100011) ?1'b1:1'b0;
+	assign dc_ins_slt = (dc_op == 6'b000000) & (dc_funt == 6'b101010) ?1'b1:1'b0;
+	assign dc_ins_sltu = (dc_op == 6'b000000) & (dc_funt == 6'b101011) ?1'b1:1'b0;
+	assign dc_ins_sll = (dc_op == 6'b000000) & (dc_funt == 6'b000000) ?1'b1:1'b0;
+	assign dc_ins_srl = (dc_op == 6'b000000) & (dc_funt == 6'b000010) ?1'b1:1'b0;
+	assign dc_ins_sra = (dc_op == 6'b000000) & (dc_funt == 6'b000011) ?1'b1:1'b0;
+	assign dc_ins_sllv = (dc_op == 6'b000000) & (dc_funt == 6'b000100) ?1'b1:1'b0;
+	assign dc_ins_srlv = (dc_op == 6'b000000) & (dc_funt == 6'b000110) ?1'b1:1'b0;
+	assign dc_ins_srav = (dc_op == 6'b000000) & (dc_funt == 6'b000111) ?1'b1:1'b0;
+	assign dc_ins_and = (dc_op == 6'b000000) & (dc_funt == 6'b100100) ?1'b1:1'b0;
+	assign dc_ins_or = (dc_op == 6'b000000) & (dc_funt == 6'b100101) ?1'b1:1'b0;
+	assign dc_ins_xor = (dc_op == 6'b000000) & (dc_funt == 6'b100110) ?1'b1:1'b0;
+	assign dc_ins_nor = (dc_op == 6'b000000) & (dc_funt == 6'b100111) ?1'b1:1'b0;
 	assign dc_type_r = dc_ins_add | dc_ins_addu | dc_ins_sub | dc_ins_subu | dc_ins_slt | dc_ins_sltu | dc_ins_sll | dc_ins_srl | dc_ins_sra | dc_ins_sllv | dc_ins_srlv | dc_ins_srav | dc_ins_and | dc_ins_or | dc_ins_xor | dc_ins_nor;
 	// R-I运算
-	assign dc_ins_addi = dc_op == 6'b001000;
-	assign dc_ins_addiu = dc_op == 6'b001001;
-	assign dc_ins_andi = dc_op == 6'b001100;
-	assign dc_ins_ori = dc_op == 6'b001101;
-	assign dc_ins_xori = dc_op == 6'b001110;
-	assign dc_ins_lui = dc_op == 6'b001111;
-	assign dc_ins_slti = dc_op == 6'b001010;
-	assign dc_ins_sltiu = dc_op == 6'b001011;
+	assign dc_ins_addi = dc_op == 6'b001000 ?1'b1:1'b0;
+	assign dc_ins_addiu = dc_op == 6'b001001 ?1'b1:1'b0;
+	assign dc_ins_andi = dc_op == 6'b001100 ?1'b1:1'b0;
+	assign dc_ins_ori = dc_op == 6'b001101 ?1'b1:1'b0;
+	assign dc_ins_xori = dc_op == 6'b001110 ?1'b1:1'b0;
+	assign dc_ins_lui = dc_op == 6'b001111 ?1'b1:1'b0;
+	assign dc_ins_slti = dc_op == 6'b001010 ?1'b1:1'b0;
+	assign dc_ins_sltiu = dc_op == 6'b001011 ?1'b1:1'b0;
 	assign dc_type_i = dc_ins_addi | dc_ins_addiu | dc_ins_andi | dc_ins_ori | dc_ins_xori | dc_ins_lui | dc_ins_slti | dc_ins_sltiu;
 	// 分支
-	assign dc_ins_beq = dc_op == 6'b000100; // a==b
-	assign dc_ins_bne = dc_op == 6'b000101; // a!=b
-	assign dc_ins_blez = dc_op == 6'b000110; // a <= 0
-	assign dc_ins_bgtz = dc_op == 6'b000111; // a > 0
-	assign dc_ins_bltz = (dc_op == 6'b000001) & (dc_reg_t == 5'b00000); // a < 0
-	assign dc_ins_bgez = (dc_op == 6'b000001) & (dc_reg_t == 5'b00001); // a >= 0
+	assign dc_ins_beq = dc_op == 6'b000100 ?1'b1:1'b0; // a==b
+	assign dc_ins_bne = dc_op == 6'b000101 ?1'b1:1'b0; // a!=b
+	assign dc_ins_blez = dc_op == 6'b000110 ?1'b1:1'b0; // a <= 0
+	assign dc_ins_bgtz = dc_op == 6'b000111 ?1'b1:1'b0; // a > 0
+	assign dc_ins_bltz = (dc_op == 6'b000001) & (dc_reg_t == 5'b00000) ?1'b1:1'b0; // a < 0
+	assign dc_ins_bgez = (dc_op == 6'b000001) & (dc_reg_t == 5'b00001) ?1'b1:1'b0; // a >= 0
 	assign dc_type_b = dc_ins_beq | dc_ins_bne | dc_ins_blez | dc_ins_bgtz | dc_ins_bltz | dc_ins_bgez;
 	assign dc_type_be = dc_ins_blez | dc_ins_bgez;
 	assign dc_type_gt = dc_ins_bgtz | dc_ins_bgez;
 	// 跳转
-	assign dc_ins_j = dc_op == 6'b000010;
-	assign dc_ins_jal = dc_op == 6'b000011;
-	assign dc_ins_jalr = (dc_op == 6'b000000) & (dc_funt == 6'b001001);
-	assign dc_ins_jr = (dc_op == 6'b000000) & (dc_funt == 6'b001000);
+	assign dc_ins_j = dc_op == 6'b000010 ?1'b1:1'b0;
+	assign dc_ins_jal = dc_op == 6'b000011 ?1'b1:1'b0;
+	assign dc_ins_jalr = (dc_op == 6'b000000) & (dc_funt == 6'b001001) ?1'b1:1'b0;
+	assign dc_ins_jr = (dc_op == 6'b000000) & (dc_funt == 6'b001000) ?1'b1:1'b0;
 	assign dc_type_jump = dc_ins_j | dc_ins_jal | dc_ins_jalr | dc_ins_jr;
 	// 传输
-	assign dc_ins_mfhi = (dc_op == 6'b000000) & (dc_funt == 6'b010000);
-	assign dc_ins_mflo = (dc_op == 6'b000000) & (dc_funt == 6'b010010);
-	assign dc_ins_mthi = (dc_op == 6'b000000) & (dc_funt == 6'b010001);
-	assign dc_ins_mtlo = (dc_op == 6'b000000) & (dc_funt == 6'b010011);
+	assign dc_ins_mfhi = (dc_op == 6'b000000) & (dc_funt == 6'b010000) ?1'b1:1'b0;
+	assign dc_ins_mflo = (dc_op == 6'b000000) & (dc_funt == 6'b010010) ?1'b1:1'b0;
+	assign dc_ins_mthi = (dc_op == 6'b000000) & (dc_funt == 6'b010001) ?1'b1:1'b0;
+	assign dc_ins_mtlo = (dc_op == 6'b000000) & (dc_funt == 6'b010011) ?1'b1:1'b0;
 
 endmodule
