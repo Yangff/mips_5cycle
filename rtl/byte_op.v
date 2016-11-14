f57da6325352d9655416ea45ef42712601cf3098
Fangfei Yang <yangff1@gmail.com>
2016-11-14 17:07:50 +0800

    half-wise

--- rtl/byte_op.v
+++ rtl/byte_op.v
@@ -1,9 +1,9 @@
 // return org[pos] = val
-// big-editon
+// big-Endian
 module byte_op(org, pos, val, result);
 
 	input [31:0] org;
-	input [2:0] pos;
+	input [3:0] pos; // SIGNED MIX/FETCH POS
 	input [7:0] val;
 
 	output [31:0] result;
@@ -11,45 +11,21 @@ module byte_op(org, pos, val, result);
 	always @(*) begin
 		result = 0;
 		case (pos)
-		  4'b100: result = {val, org[23:0]}; // 00 - lower memory, higher data 
-		  4'b101: result = {org[31:24], val, org[15:0]};
-		  4'b110: result = {org[31:16], val, org[7:0]};
-		  4'b111: result = {org[31:8], val}; 
+		  4'b0100: result = {val, org[23:0]}; // 00 - lower memory, higher data 
+		  4'b0101: result = {org[31:24], val, org[15:0]};
+		  4'b0110: result = {org[31:16], val, org[7:0]};
+		  4'b0111: result = {org[31:8], val}; 
 
-		  4'b000: result = org[31:24];
-		  4'b001: result = org[23:16];
-		  4'b010: result = org[15:8];
-		  4'b011: result = org[7:0];
+		  4'b0000: result = {24'b0, org[31:24]};
+		  4'b0001: result = {24'b0, org[23:16]};
+		  4'b0010: result = {24'b0, org[15:8]};
+		  4'b0011: result = {24'b0, org[7:0]};
+
+		  4'b1000: result = {{24{org[31]}}, org[31:24]};
+		  4'b1001: result = {{24{org[23]}}, org[23:16]};
+		  4'b1010: result = {{24{org[15]}}, org[15:8]};
+		  4'b1011: result = {{24{org[7]}}, org[7:0]};
 		  default: result = 0;
 		endcase
 	end
 endmodule
-
-/*
-
-little-editon
-
-module byte_op(org, pos, val, result);
-
-	input [31:0] org;
-	input [2:0] pos;
-	input [7:0] val;
-
-	output [31:0] result;
-
-	always @(*) begin
-		case (pos) begin
-		  4'b100: result = {org[31:8], val};; // 00 - lower memory, lower data 
-		  4'b101: result = {org[31:16], val, org[7:0]};
-		  4'b110: result = {org[31:24], val, org[15:0]};
-		  4'b111: result = {val, org[23:0]}
-		  4'b000: result = org[7:0];
-		  4'b001: result = org[15:8];
-		  4'b010: result = org[23:16];
-		  4'b011: result = org[31:24];
-		default: result = 0;
-	  endcase
-	end
-endmodule
-
-*/
---
+++ rtl/half_op.v
@@ -0,0 +1,24 @@
+// return org[pos] = val
+// big-Endian
+module half_op(org, pos, val, result);
+
+	input [31:0] org;
+	input [2:0] pos; // SIGNED MIX/FETCH POS
+	input [15:0] val;
+
+	output [31:0] result;
+
+	always @(*) begin
+		result = 0;
+		case (pos)
+		  4'b010: result = {val, org[15:0]}; // 00 - lower memory, higher data 
+		  4'b011: result = {org[31:16], val}; 
+
+		  4'b000: result = {16'b0, org[31:16]};
+		  4'b001: result = {16'b0, org[15:0]};
+		  4'b100: result = {{16{org[31]}}, org[31:16]};
+		  4'b101: result = {{16{org[15]}}, org[15:0]};
+		  default: result = 0;
+		endcase
+	end
+endmodule
